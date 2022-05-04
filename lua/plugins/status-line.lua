3672f9409b137c7edcee6607a43f17b3c5affe0b
pujichun <pujichun@outlook.com>
2022-05-04 16:14:27 +0800

    style: setting statusline

--- lua/plugins/status-line.lua
+++ lua/plugins/status-line.lua
@@ -1,32 +1,32 @@
-local custom_theme = require'lualine.themes.ayu_mirage'
+local custom_theme = require 'lualine.themes.ayu_mirage'
 custom_theme.normal.c.bg = '#1f2735'
 
-require('lualine').setup {
+require('lualine').setup({
   options = {
     icons_enabled = true,
     theme = custom_theme,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "dashboard", "NvimTree" },
     always_divide_middle = true,
     globalstatus = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
   extensions = {}
-}
+})
