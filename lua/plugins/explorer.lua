06411711a1821a156ad02bb270ff76f172721cad
pujichun <pujichun@outlook.com>
2022-08-26 00:28:15 +0800

    版本更新

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,65 +1,46 @@
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
 local keybind = {
-  { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-  { key = "<C-e>", action = "edit_in_place" },
-  { key = { "O" }, action = "edit_no_picker" },
-  { key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
-  { key = "<C-v>", action = "vsplit" },
-  { key = "<C-x>", action = "split" },
-  { key = "<C-t>", action = "tabnew" },
-  { key = "<", action = "prev_sibling" },
-  { key = ">", action = "next_sibling" },
-  { key = "P", action = "parent_node" },
-  { key = "<BS>", action = "close_node" },
-  { key = "<Tab>", action = "preview" },
-  { key = "K", action = "first_sibling" },
-  { key = "J", action = "last_sibling" },
-  { key = "I", action = "toggle_git_ignored" },
-  { key = "H", action = "toggle_dotfiles" },
-  { key = "R", action = "refresh" },
-  { key = "a", action = "create" },
-  { key = "d", action = "remove" },
-  { key = "D", action = "trash" },
-  { key = "r", action = "rename" },
-  { key = "<C-r>", action = "full_rename" },
-  { key = "x", action = "cut" },
-  { key = "c", action = "copy" },
-  { key = "p", action = "paste" },
-  { key = "y", action = "copy_name" },
-  { key = "Y", action = "copy_path" },
-  { key = "gy", action = "copy_absolute_path" },
-  { key = "[c", action = "prev_git_item" },
-  { key = "]c", action = "next_git_item" },
-  { key = "-", action = "dir_up" },
-  { key = "s", action = "system_open" },
-  { key = "q", action = "close" },
-  { key = "g?", action = "toggle_help" },
-  { key = "W", action = "collapse_all" },
-  { key = "S", action = "search_node" },
-  { key = "<C-k>", action = "toggle_file_info" },
-  { key = ".", action = "run_file_command" }
+  {key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit"},
+  {key = "<C-e>", action = "edit_in_place"},
+  {key = {"O"}, action = "edit_no_picker"},
+  {key = {"<2-RightMouse>", "<C-]>"}, action = "cd"},
+  {key = "<C-v>", action = "vsplit"},
+  {key = "<C-x>", action = "split"},
+  {key = "<C-t>", action = "tabnew"},
+  {key = "<", action = "prev_sibling"},
+  {key = ">", action = "next_sibling"},
+  {key = "P", action = "parent_node"},
+  {key = "<BS>", action = "close_node"},
+  {key = "<Tab>", action = "preview"},
+  {key = "K", action = "first_sibling"},
+  {key = "J", action = "last_sibling"},
+  {key = "I", action = "toggle_git_ignored"},
+  {key = "H", action = "toggle_dotfiles"},
+  {key = "R", action = "refresh"},
+  {key = "a", action = "create"},
+  {key = "d", action = "remove"},
+  {key = "D", action = "trash"},
+  {key = "r", action = "rename"},
+  {key = "<C-r>", action = "full_rename"},
+  {key = "x", action = "cut"},
+  {key = "c", action = "copy"},
+  {key = "p", action = "paste"},
+  {key = "y", action = "copy_name"},
+  {key = "Y", action = "copy_path"},
+  {key = "gy", action = "copy_absolute_path"},
+  {key = "[c", action = "prev_git_item"},
+  {key = "]c", action = "next_git_item"},
+  {key = "-", action = "dir_up"},
+  {key = "s", action = "system_open"},
+  {key = "q", action = "close"},
+  {key = "g?", action = "toggle_help"},
+  {key = "W", action = "collapse_all"},
+  {key = "S", action = "search_node"},
+  {key = "<C-k>", action = "toggle_file_info"},
+  {key = ".", action = "run_file_command"}
 }
 
-require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
+require "nvim-tree".setup {
+  -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
   disable_netrw = false,
   hijack_cursor = false,
@@ -82,8 +63,8 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     signcolumn = "yes",
     mappings = {
       custom_only = false,
-      list = keybind,
-    },
+      list = keybind
+    }
   },
   renderer = {
     indent_markers = {
@@ -91,26 +72,46 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       icons = {
         corner = "└ ",
         edge = "│ ",
-        none = "  ",
-      },
+        none = "  "
+      }
     },
     icons = {
       webdev_colors = true,
-    },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌"
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = ""
+        }
+      }
+    }
   },
   hijack_directories = {
     enable = true,
-    auto_open = true,
+    auto_open = true
   },
   update_focused_file = {
     enable = false,
     update_cwd = false,
-    ignore_list = {},
+    ignore_list = {}
   },
   ignore_ft_on_setup = {},
   system_open = {
     cmd = "",
-    args = {},
+    args = {}
   },
   diagnostics = {
     enable = false,
@@ -119,25 +120,25 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       hint = "",
       info = "",
       warning = "",
-      error = "",
-    },
+      error = ""
+    }
   },
   filters = {
     dotfiles = false,
     custom = {},
-    exclude = {},
+    exclude = {}
   },
   git = {
     enable = true,
     ignore = true,
-    timeout = 400,
+    timeout = 400
   },
   actions = {
     use_system_clipboard = true,
     change_dir = {
       enable = true,
       global = false,
-      restrict_above_cwd = false,
+      restrict_above_cwd = false
     },
     open_file = {
       quit_on_open = false,
@@ -146,15 +147,15 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
         enable = true,
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
         exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
+          filetype = {"notify", "packer", "qf", "diff", "fugitive", "fugitiveblame"},
+          buftype = {"nofile", "terminal", "help"}
+        }
+      }
+    }
   },
   trash = {
     cmd = "trash",
-    require_confirm = true,
+    require_confirm = true
   },
   log = {
     enable = false,
@@ -165,7 +166,7 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       copy_paste = false,
       diagnostics = false,
       git = false,
-      profile = false,
-    },
-  },
+      profile = false
+    }
+  }
 }
